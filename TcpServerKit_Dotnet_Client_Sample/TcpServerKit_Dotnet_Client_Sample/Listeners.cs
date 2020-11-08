592ba3e9eb8aaa8662acfa34c9735de169050b14
hosein asadi <work.hoseinasadi@gmail.com>
2020-11-08 12:11:55 +0330

    Add files via upload

---
+++ TcpServerKit_Dotnet_Client_Sample/TcpServerKit_Dotnet_Client_Sample/Listeners.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TcpServerKit_Dotnet_Client_Sample
+{
+    public static class Listeners
+    {
+        internal static void Join(string data)
+        {
+            Console.WriteLine($"{data} from Joint");
+        }
+    }
+}
